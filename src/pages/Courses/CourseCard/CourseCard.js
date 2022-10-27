dce11c4ba5a279634d5830bd13c06c2b7aba1b7d
MRPrio <maksudurprio.mp@gmail.com>
2022-10-27 14:12:46 +0600

    course cards section responsived

--- src/pages/Courses/CourseCard/CourseCard.js
+++ src/pages/Courses/CourseCard/CourseCard.js
@@ -5,8 +5,8 @@ import './CourseCard.css'
 const CourseCard = ({ CourseCard }) => {
   const { title, thumbnail_url, details } = CourseCard;
   return (
-    <div className="d-flex justify-content-center align-items-center my-5 cards ">
-      <Image rounded style={{width:'40%'}} src={thumbnail_url}></Image>
+    <div className="d-flex course-cards justify-content-center align-items-center my-5 cards ">
+      <Image  rounded style={{width:'40%'}} src={thumbnail_url}></Image>
       <div className="ms-3">
         <h5>{title}</h5>
         <p>{details.slice(0,200) + '...'}</p>
