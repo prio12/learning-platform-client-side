e9d146e620742c2c5a5220f068a4be6d2a7e334b
MRPrio <maksudurprio.mp@gmail.com>
2022-10-27 14:46:21 +0600

    course details section responsived

--- src/pages/CourseDetails/PDF/PdfDownload.js
+++ src/pages/CourseDetails/PDF/PdfDownload.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Pdf from "react-to-pdf";
 import { Button } from 'react-bootstrap';
 import { HiBookmark } from "react-icons/hi";
+import './PdfDownload.css'
 
 const ref = React.createRef();
 
@@ -10,17 +11,17 @@ const PdfDownload = ({pdfDetails}) => {
         <div>
             <div ref={ref}>
                 <div className='text-center'>
-                <h4>This course includes:</h4>
-                <div className='text-center'>
+                <h4 className='course-includes-title'>This course includes:</h4>
+                <div className='text-center course-includes'>
                 <div className='d-flex justify-content-center'>
-                <span><HiBookmark/>2.5 hours on-demand video</span><br />
-                <span><HiBookmark/>Access on mobile and TV</span><br />
+                <span className='course-includes'><HiBookmark/>2.5 hours on-demand video</span><br />
+                <span className='course-includes'><HiBookmark/>Access on mobile and TV</span><br />
                 </div>
                 <div className='d-flex justify-content-center'>
-                <span><HiBookmark/>1 downloadable resource</span><br />
-                <span><HiBookmark/>Certificate of completion</span>
+                <span className='course-includes'><HiBookmark/>1 downloadable resource</span><br />
+                <span className='course-includes'><HiBookmark/>Certificate of completion</span>
                 </div>
-                <span><HiBookmark/>Full lifetime access</span><br />
+                <span className='course-includes'><HiBookmark/>Full lifetime access</span><br />
                 </div>
                 </div>
       </div>
